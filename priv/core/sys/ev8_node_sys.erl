2277e9c5a221e995fcd7eab0befb5b9605d70a4f
Hollin Wilkins <chromaticbum@gmail.com>
2012-12-27 00:51:13 -0800

    Move methods into util from sys.

--- priv/core/sys/ev8_node_sys.erl
+++
@@ -1,8 +0,0 @@
--module(ev8_node_sys).
-
--export([
-  error/2
-  ]).
-
-error(_, Errors) ->
-  {error, {sys_error, Errors}}.
--- priv/core/util/ev8_node_util.erl
+++ priv/core/util/ev8_node_util.erl
@@ -1,1 +1,8 @@
 -module(ev8_node_util).
+
+-export([
+  error/2
+  ]).
+
+error(_, Errors) ->
+  {error, {sys_error, Errors}}.
